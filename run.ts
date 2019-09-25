172d5142c414cb56b71ee917bdca5c8c5002fcda
Alexey Zauzin <lehadnk@gmail.com>
2019-09-25 17:53:01 +0300

    Initial commit

---
+++ run.ts
@@ -0,0 +1,32 @@
+import {config as dotenvInit} from 'dotenv';
+import {Client, TextChannel} from "discord.js";
+
+dotenvInit();
+
+let rand = Math.random() * 6;
+if (rand <= 1) {
+    console.log('test');
+}
+
+let discordClient = new Client();
+const muteTime = parseInt(process.env.MUTE_TIME) * 1000;
+
+discordClient.on("message", async msg => {
+    if (msg.member.hasPermission("ADMINISTRATOR")) {
+        msg.channel.send("Сорян, бро, админы в игре не учавствуют.");
+        return;
+    }
+
+    if (msg.content != '!рулетка') return;
+
+    let rand = Math.random() * 6;
+    if (rand <= 1) {
+        msg.member.addRole(process.env.MUTE_ROLE);
+        msg.channel.send("**ВЫСТРЕЛ КОЛЬТА В ТУПОЕ ЕБЛО <@" + msg.author.id + ">! ЭТОТ ПАРЕНЬ ЗНАЛ НА ЧТО ШЕЛ И ПОЛУЧИЛ СВОЙ МУТ НА 24 ЧАСА**");
+        console.log('Added mute to: ' + msg.member.id);
+        setTimeout(() => {
+            msg.member.removeRole(process.env.MUTE_ROLE);
+        }, muteTime);
+    }
+});
+discordClient.login(process.env.DISCORD_BOT_TOKEN);
